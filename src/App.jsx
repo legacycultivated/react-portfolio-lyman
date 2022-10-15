92b7a3e5541bae5732854bc34615b8a98d4cf339
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-10-14 17:28:37 -0700

    create components and import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Projects from "./components/projects/Projects";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
