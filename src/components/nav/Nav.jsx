0ff30660ad6f6394c184cb166e789c67d0dc5789
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-10-18 15:58:19 -0700

    edit header and create nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { BiUserCircle, BiMessageAltDetail } from "react-icons/bi";
+import { ImBooks } from "react-icons/im";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <a href="#">
+        <AiOutlineHome />
+      </a>
+      <a href="#about">
+        <BiUserCircle />
+      </a>
+      <a href="#projects">
+        <ImBooks />
+      </a>
+      <a href="#contact">
+        <BiMessageAltDetail />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
