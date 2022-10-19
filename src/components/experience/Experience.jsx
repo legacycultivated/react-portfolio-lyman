b660bd54c378196e185a3284dad79d18287681d9
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-10-18 19:54:04 -0700

    create experience component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -5,66 +5,130 @@ import { BsPatchCheckFill } from "react-icons/bs";
 const Experience = () => {
   return (
     <section id="experience">
-      <h5>What Skills I have</h5>
+      <h5>Current Skillset</h5>
       <h2>My Experience</h2>
 
       <div className="container experience_container">
         <div className="current_technologies">
           <h3>Current Languages/Tech I'm Using</h3>
-          <div className="experience_content"></div>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>HTML</h4>
+              </div>
+            </article>
 
-          <article className="experience_details"></article>
-          <BsPatchCheckFill />
-          <h4>HTML</h4>
-          <article className="experience_details"></article>
-          <BsPatchCheckFill />
-          <h4>CSS/SCSS/SASS</h4>
-          <article className="experience_details"></article>
-          <BsPatchCheckFill />
-          <h4>JavaScript</h4>
-          <article className="experience_details"></article>
-          <BsPatchCheckFill />
-          <h4>React.js</h4>
-          <article className="experience_details"></article>
-          <BsPatchCheckFill />
-          <h4>Redux</h4>
-          <article className="experience_details"></article>
-          <BsPatchCheckFill />
-          <h4>Bootstrap</h4>
-          <article className="experience_details"></article>
-          <BsPatchCheckFill />
-          <h4>Node.js</h4>
-          <article className="experience_details"></article>
-          <BsPatchCheckFill />
-          <h4>Git</h4>
-          <article className="experience_details"></article>
-          <BsPatchCheckFill />
-          <h4>Github</h4>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>CSS/SCSS/SASS</h4>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Bootstrap</h4>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>React.js</h4>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Redux</h4>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Node.js</h4>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Git</h4>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Github</h4>
+              </div>
+            </article>
+          </div>
         </div>
-      </div>
-      <div className="experience_content">
-        <h3>Languages/Tech Used Previously</h3>
-        <article className="experience_details"></article>
-        <BsPatchCheckFill />
-        <h4>SQL</h4>
-        <article className="experience_details"></article>
-        <BsPatchCheckFill />
-        <h4>Python</h4>
-        <article className="experience_details"></article>
-        <BsPatchCheckFill />
-        <h4>MongoDB</h4>
-        <article className="experience_details"></article>
-        <BsPatchCheckFill />
-        <h4>AWS (Lambda)</h4>
-        <article className="experience_details"></article>
-        <BsPatchCheckFill />
-        <h4>Google Cloud Platform</h4>
-        <article className="experience_details"></article>
-        <BsPatchCheckFill />
-        <h4>jQuery</h4>
-        <article className="experience_details"></article>
-        <BsPatchCheckFill />
-        <h4>Express</h4>
+        <div className="previous_technologies">
+          <h3>Languages/Tech Used Previously</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Python</h4>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>MongoDB</h4>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>AWS (Lambda)</h4>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Google Cloud Platform</h4>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>jQuery</h4>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>Express</h4>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details-icon" />
+              <div>
+                <h4>RESTful API</h4>
+              </div>
+            </article>
+          </div>
+        </div>
       </div>
     </section>
   );
