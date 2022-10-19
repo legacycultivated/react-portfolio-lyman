c53ff3650a4613d223aba8df5d7618e1cb413031
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-10-18 17:39:47 -0700

    create about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "./about.css";
 import ME from "../../assets/Avatar.svg";
-import { BsTrophyFill, BsInfoCircleFill } from "react-icons/bs";
-import { ImBooks } from "react-icons/im";
+import { BsTrophyFill, BsInfoCircleFill, BsFolderFill } from "react-icons/bs";
 
 const About = () => {
   return (
@@ -25,9 +24,9 @@ const About = () => {
               <small>Junior Developer</small>
             </article>
             <article className="about_card">
-              <ImBooks className="about_icon" />
+              <BsFolderFill className="about_icon" />
               <h5>Projects</h5>
-              <small>10+ Projects</small>
+              <small>10+ Completed Projects</small>
             </article>
             <article className="about_card">
               <BsInfoCircleFill className="about_icon" />
@@ -35,6 +34,16 @@ const About = () => {
               <small>Hobbies and Interests</small>
             </article>
           </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+            quibusdam voluptas quis aut et mollitia id debitis sequi, nihil eius
+            vitae ad ipsam vero repellendus architecto aperiam nostrum neque
+            optio.
+          </p>
+          <a href="contact" className="btn btn-primary">
+            Contact Me
+          </a>
         </div>
       </div>
     </section>
