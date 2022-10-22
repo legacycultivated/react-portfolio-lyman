f7e9ce7b32de08a0f37817601d423ef21168ce13
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-10-21 17:09:41 -0700

    add project pics and begin projects component

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,7 +2,26 @@ import React from "react";
 import "./projects.css";
 
 const Projects = () => {
-  return <section id="projects">Projects</section>;
+  return (
+    <section id="projects">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio_container">
+        <article className="portfolio_item">
+          <div className="portfolio_item-image"></div>
+
+          <h3>This is a portfolio item title</h3>
+          <a href="github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a href="#home" className="btn btn-primary" target="_blank">
+            Live Demo
+          </a>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
