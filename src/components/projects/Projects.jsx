a4306c464807e2c8e0a8108743f8ca6fb40cf856
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-10-24 15:07:10 -0700

    create projects arrays

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,56 @@
 import React from "react";
 import "./projects.css";
 import IMG1 from "../../assets/meet-1.png";
+import IMG2 from "../../assets/myFlixScreenshot.png";
+import IMG3 from "../../assets/pokeApi.png";
+import IMG4 from "../../assets/tindog-1.png";
+import IMG5 from "../../assets/wireframe.jpg";
+import IMG6 from "../../assets/code-pic.jpg";
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Meet Project Application",
+    github: "https://github.com/legacycultivated/meet-app",
+    demo: "https://legacycultivated.github.io/meet-app/",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "myFlix Movie Application",
+    github: "https://github.com/legacycultivated/myFlix-client",
+    demo: "",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "PokeAPI Project",
+    github: "https://github.com/legacycultivated/pokedex-js-project",
+    demo: " https://legacycultivated.github.io/pokedex-js-project/",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "TinDog Landing Page",
+    github: "https://github.com/legacycultivated/TinDogLandingPage",
+    demo: "https://legacycultivated.github.io/TinDogLandingPage/",
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: "Bootstrap Web Developer Portfolio",
+    github: "https://github.com/MJTCdev/Portfolio",
+    demo: "#",
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "Youtube App",
+    github: "#",
+    demo: "#",
+  },
+];
 
 const Projects = () => {
   return (
