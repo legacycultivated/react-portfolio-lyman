fa05dc278706dc206f444b9233bbd8ee9427fbf2
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-10-24 17:09:54 -0700

    update contact css component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
       <div className="container contact_container">
         <div className="contact_options">
           <article className="contact_option">
-            <AiOutlineMail />
+            <AiOutlineMail className="contact_option-icon" />
             <h4>Email</h4>
             <h5>lymanj.chan@gmail.com</h5>
             <a
@@ -24,7 +24,7 @@ const Contact = () => {
             </a>
           </article>
           <article className="contact_option">
-            <AiFillLinkedin />
+            <AiFillLinkedin className="contact_option-icon" />
             <h4>LinkedIn</h4>
             <h5>Join My Network</h5>
             <a
@@ -36,13 +36,30 @@ const Contact = () => {
             </a>
           </article>
           <article className="contact_option">
-            <GoLocation />
+            <GoLocation className="contact_option-icon" />
             <h4>Location</h4>
             <h5>Las Vegas, Nevada</h5>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form action=""></form>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   );
